668ca559bee6f4515f9c26faa627a8a1eceac751
stoxhorn <33036381+stoxhorn@users.noreply.github.com>
2018-05-09 16:56:48 +0200

    adesf

--- src/compression/Main.java
+++ src/compression/Main.java
@@ -5,6 +5,28 @@ import java.util.*;
 
 public class Main {
 
+    private static void printFreq(int[] list)
+    {
+        ArrayList<String> arList = new ArrayList();
+        int i = 0;
+        String tmp;
+        for(int str : list)
+        {
+            tmp = String.valueOf(i);
+            if(str == 0)
+            {
+            }
+            else
+            {
+                tmp += ": ";
+                tmp += String.valueOf(str);
+                arList.add(tmp);
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(arList.toArray()));
+    }
+    
     public static void main(String[] args) {
         try {
             // String for testing, remember to take index 0 from arguments when finished
@@ -19,24 +41,7 @@ public class Main {
 				// Tjek resultat med https://www.asciitable.com/
 
             }
-            ArrayList<String> arList = new ArrayList();
-            int i = 0;
-            String tmp;
-            for(int str : list)
-            {
-                tmp = String.valueOf(i);
-                if(str == 0)
-                {
-                }
-                else
-                {
-                    tmp += ": ";
-                    tmp += String.valueOf(str);
-                    arList.add(tmp);
-                }
-                i++;
-            }
-            System.out.println(Arrays.toString(arList.toArray()));
+        printFreq(list);
 			
 			
 
