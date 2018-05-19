4101b5217a6ed535f26c8dcf3601128e5021995b
stoxhorn <33036381+stoxhorn@users.noreply.github.com>
2018-05-19 14:22:24 +0200

    asdd

--- src/compression/HuffTree.java
+++ src/compression/HuffTree.java
@@ -22,79 +22,28 @@ public class HuffTree {
         PQHeap q = heap;
         for(int i = 1; i < n; i++)
         {
-            
+            // New Parentnode
             HuffNode z = new HuffNode();
+            
+            // Left child
             HuffNode x = new HuffNode(q.extractMin());
             z.setLchild(x);
+            
+            // Right child
             HuffNode y = new HuffNode(q.extractMin());
             z.setRchild(y);
             
+            // Set frequency of new child
             z.setFreq(x.getFreq() + y.getFreq());
-            //System.out.println(x.getFreq() + y.getFreq());
-            //System.out.println(z.getFreq());
-            q.insert(new Element(z.getFreq(), z));
-            q.printHeap();
             
+            // Insert to heap
+            q.insert(new Element(z.getFreq(), z));
         }
-        //System.out.println(q.getSize());
         return q.extractMin();
     }
-    
-    
-    /*
-    Pseudo-kode for Huffmantree
-    HUFFMAN(C)
-    n = |C|
-    Q = C
-    for i = 1 to n-1
-        allocate a new node z
-        z.left = x = EXTRACT-MIN(Q)
-        z.rigth = y = EXTRACT-MIN(Q)
-        z.freq = x.freq + y.freq
-        INSERT(Q,z)
-    return EXTRACT-MIN(Q)
-    */
-        
-    
-    
-    
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+}
     
     
     
